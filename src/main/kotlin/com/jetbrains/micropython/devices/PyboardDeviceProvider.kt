7b9c84dc35dfe6bee54feaf16df6c34e25c098c3
Andrey Vlasovskikh <andrey.vlasovskikh@gmail.com>
2017-09-05 18:19:20 +0300

    License and formatting

--- src/main/kotlin/com/jetbrains/micropython/devices/PyboardDeviceProvider.kt
+++ src/main/kotlin/com/jetbrains/micropython/devices/PyboardDeviceProvider.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2000-2017 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.jetbrains.micropython.devices
 
 import com.intellij.execution.configurations.CommandLineState
@@ -15,41 +31,40 @@ import com.jetbrains.python.packaging.PyRequirement
  * @author stefanhoelzl
  */
 class PyboardDeviceProvider : MicroPythonDeviceProvider {
-    override val persistentName: String
-        get() = "Pyboard"
+  override val persistentName: String
+    get() = "Pyboard"
 
-    override val documentationURL: String
-        get() = "https://github.com/vlasovskikh/intellij-micropython/wiki/Pyboard"
+  override val documentationURL: String
+    get() = "https://github.com/vlasovskikh/intellij-micropython/wiki/Pyboard"
 
-    override val usbIds: List<MicroPythonUsbId>
-        get() = listOf(MicroPythonUsbId(0xF055, 0x9800))
+  override val usbIds: List<MicroPythonUsbId>
+    get() = listOf(MicroPythonUsbId(0xF055, 0x9800))
 
-    override val typeHints: MicroPythonTypeHints by lazy {
-        MicroPythonTypeHints(listOf("stdlib", "micropython"))
+  override val typeHints: MicroPythonTypeHints by lazy {
+    MicroPythonTypeHints(listOf("stdlib", "micropython"))
+  }
+
+  override val packageRequirements: List<PyRequirement> by lazy {
+    PyRequirement.fromText("""|pyserial>=3.3,<3.4
+                              |docopt>=0.6.2,<0.7
+                              |adafruit-ampy>=1.0.1,<1.1""".trimMargin())
+  }
+
+  override fun getRunCommandLineState(configuration: MicroPythonRunConfiguration,
+                                      environment: ExecutionEnvironment): CommandLineState? {
+    val facet = configuration.module?.microPythonFacet ?: return null
+    val pythonPath = facet.pythonPath ?: return null
+    val devicePath = facet.devicePath ?: return null
+    val rootPath = configuration.project.basePath ?: return null
+    return object : CommandLineState(environment) {
+      override fun startProcess() =
+          OSProcessHandler(GeneralCommandLine(pythonPath,
+                                              "${MicroPythonFacet.scriptsPath}/microupload.py",
+                                              "-C",
+                                              rootPath,
+                                              "-v",
+                                              devicePath,
+                                              configuration.path))
     }
-
-    override val packageRequirements: List<PyRequirement> by lazy {
-        PyRequirement.fromText("""pyserial>=3.3,<3.4
-            |docopt>=0.6.2
-            |adafruit-ampy>=1.0.1
-        """.trimMargin())
-    }
-
-    override fun getRunCommandLineState(configuration: MicroPythonRunConfiguration,
-                                        environment: ExecutionEnvironment): CommandLineState? {
-        val facet = configuration.module?.microPythonFacet ?: return null
-        val pythonPath = facet.pythonPath ?: return null
-        val devicePath = facet.devicePath ?: return null
-        val rootPath = configuration.project.basePath ?: return null
-        return object : CommandLineState(environment) {
-            override fun startProcess() =
-                    OSProcessHandler(GeneralCommandLine(pythonPath,
-                            "${MicroPythonFacet.scriptsPath}/microupload.py",
-                            "-C",
-                            rootPath,
-                            "-v",
-                            devicePath,
-                            configuration.path))
-        }
-    }
+  }
 }
