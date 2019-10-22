f0d49294d051c6a3b949c67a51c9b51b4b266291
Marvin H. Sielenkemper <sielenk@gmx.de>
2019-10-22 21:03:55 +0200

    fixed the Pyboard device provider to respect the excluded folders

--- src/main/kotlin/com/jetbrains/micropython/devices/PyboardDeviceProvider.kt
+++ src/main/kotlin/com/jetbrains/micropython/devices/PyboardDeviceProvider.kt
@@ -21,6 +21,10 @@ import com.intellij.execution.configurations.GeneralCommandLine
 import com.intellij.execution.process.OSProcessHandler
 import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.openapi.projectRoots.Sdk
+import com.intellij.openapi.roots.ModuleRootManager
+import com.intellij.openapi.vfs.StandardFileSystems
+import com.intellij.openapi.vfs.VfsUtil
+import com.intellij.openapi.vfs.VfsUtilCore
 import com.jetbrains.micropython.run.MicroPythonRunConfiguration
 import com.jetbrains.micropython.settings.MicroPythonFacet
 import com.jetbrains.micropython.settings.MicroPythonTypeHints
@@ -55,19 +59,28 @@ class PyboardDeviceProvider : MicroPythonDeviceProvider {
 
   override fun getRunCommandLineState(configuration: MicroPythonRunConfiguration,
                                       environment: ExecutionEnvironment): CommandLineState? {
-    val facet = configuration.module?.microPythonFacet ?: return null
+    val module = configuration.module ?: return null
+    val facet = module.microPythonFacet ?: return null
     val pythonPath = facet.pythonPath ?: return null
     val devicePath = facet.devicePath ?: return null
     val rootPath = configuration.project.basePath ?: return null
+    val rootDir = StandardFileSystems.local().findFileByPath(rootPath) ?: return null
+    val file = StandardFileSystems.local().findFileByPath(configuration.path) ?: return null
+    val excludeRoots = ModuleRootManager.getInstance(module).excludeRoots
+    val excludes = excludeRoots
+        .asSequence()
+        .filter { VfsUtil.isAncestor(file, it, false) }
+        .map { VfsUtilCore.getRelativePath(it, rootDir) }
+        .filterNotNull()
+        .map { listOf("-X", it) }
+        .flatten()
+        .toList()
+    val command = listOf(pythonPath, "${MicroPythonFacet.scriptsPath}/microupload.py", "-C", rootPath) +
+        excludes + listOf("-v", devicePath, configuration.path)
+
     return object : CommandLineState(environment) {
       override fun startProcess() =
-          OSProcessHandler(GeneralCommandLine(pythonPath,
-                                              "${MicroPythonFacet.scriptsPath}/microupload.py",
-                                              "-C",
-                                              rootPath,
-                                              "-v",
-                                              devicePath,
-                                              configuration.path))
+          OSProcessHandler(GeneralCommandLine(command))
     }
   }
 }
