38ecab7c6afd7f3baece2176ee008ba14ed1f8b6
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2020-06-06 15:53:38 +0300

    ESP8266: Flash files and folders relative to the closest source or content root #81

--- src/main/kotlin/com/jetbrains/micropython/devices/Esp8266DeviceProvider.kt
+++ src/main/kotlin/com/jetbrains/micropython/devices/Esp8266DeviceProvider.kt
@@ -4,11 +4,15 @@ import com.intellij.execution.configurations.CommandLineState
 import com.intellij.execution.configurations.GeneralCommandLine
 import com.intellij.execution.process.OSProcessHandler
 import com.intellij.execution.runners.ExecutionEnvironment
+import com.intellij.openapi.module.Module
+import com.intellij.openapi.project.guessModuleDir
+import com.intellij.openapi.project.rootManager
 import com.intellij.openapi.projectRoots.Sdk
 import com.intellij.openapi.roots.ModuleRootManager
 import com.intellij.openapi.vfs.StandardFileSystems
 import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.vfs.VfsUtilCore
+import com.intellij.openapi.vfs.VirtualFile
 import com.jetbrains.micropython.run.MicroPythonRunConfiguration
 import com.jetbrains.micropython.settings.MicroPythonFacet
 import com.jetbrains.micropython.settings.MicroPythonTypeHints
@@ -45,27 +49,49 @@ class Esp8266DeviceProvider : MicroPythonDeviceProvider {
   override fun getRunCommandLineState(configuration: MicroPythonRunConfiguration,
                                       environment: ExecutionEnvironment): CommandLineState? {
     val module = configuration.module ?: return null
-    val facet = module.microPythonFacet ?: return null
-    val pythonPath = facet.pythonPath ?: return null
-    val devicePath = facet.devicePath ?: return null
-    val rootPath = configuration.project.basePath ?: return null
-    val rootDir = StandardFileSystems.local().findFileByPath(rootPath) ?: return null
-    val file = StandardFileSystems.local().findFileByPath(configuration.path) ?: return null
-    val excludeRoots = ModuleRootManager.getInstance(module).excludeRoots
-    val excludes = excludeRoots
-        .asSequence()
-        .filter { VfsUtil.isAncestor(file, it, false) }
-        .map { VfsUtilCore.getRelativePath(it, rootDir) }
-        .filterNotNull()
-        .map { listOf("-X", it) }
-        .flatten()
-        .toList()
-    val command = listOf(pythonPath, "${MicroPythonFacet.scriptsPath}/microupload.py", "-C", rootPath) +
-        excludes + listOf("-v", devicePath, configuration.path)
+    val command = getMicroUploadCommand(configuration.path, module) ?: return null
 
     return object : CommandLineState(environment) {
       override fun startProcess() =
           OSProcessHandler(GeneralCommandLine(command))
+    }
+  }
+
+  companion object {
+    private fun getMicroUploadCommand(path: String, module: Module): List<String>? {
+      val facet = module.microPythonFacet ?: return null
+      val pythonPath = facet.pythonPath ?: return null
+      val devicePath = facet.devicePath ?: return null
+      val file = StandardFileSystems.local().findFileByPath(path) ?: return null
+      val rootDir = getClosestRoot(file, module) ?: return null
+      val excludeRoots = ModuleRootManager.getInstance(module).excludeRoots
+      val excludes = excludeRoots
+          .asSequence()
+          .filter { VfsUtil.isAncestor(file, it, false) }
+          .map { VfsUtilCore.getRelativePath(it, rootDir) }
+          .filterNotNull()
+          .map { listOf("-X", it) }
+          .flatten()
+          .toList()
+      return listOf(pythonPath, "${MicroPythonFacet.scriptsPath}/microupload.py", "-C", rootDir.path) +
+          excludes +
+          listOf("-v", devicePath, path)
+    }
+
+    private fun getClosestRoot(file: VirtualFile, module: Module): VirtualFile? {
+      val roots = mutableSetOf<VirtualFile>().apply {
+        val rootManager = module.rootManager
+        addAll(rootManager.contentRoots)
+        addAll(rootManager.sourceRoots)
+      }
+      var parent: VirtualFile? = file
+      while (parent != null) {
+        if (parent in roots) {
+          break
+        }
+        parent = parent.parent
+      }
+      return parent ?: module.guessModuleDir()
     }
   }
 }
