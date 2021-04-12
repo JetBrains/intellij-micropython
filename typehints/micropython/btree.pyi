52674f93c95a3784266e3ae3cea59d16ba752e1f
Howard Lovatt <howard.lovatt@gmail.com>
2021-04-12 12:47:15 +1000

    Update btree.pyi
    Generated by https://github.com/hlovatt/PyBoardTypeshedGenerator.

--- typehints/micropython/btree.pyi
+++ typehints/micropython/btree.pyi
@@ -1,4 +1,14 @@
-"""simple BTree database
+"""
+
+simple BTree database
+
+Descriptions taken from 
+`https://raw.githubusercontent.com/micropython/micropython/master/docs/library/btree.rst`, etc.
+
+=====================================
+
+.. module:: btree
+   :synopsis: simple BTree database
 
 The ``btree`` module implements a simple key-value database using external
 storage (disk files, or in general case, a random-access `stream`). Keys are
@@ -67,60 +77,249 @@ Example::
 
     # Don't forget to close the underlying stream!
     f.close()
+
+   
+
 """
 
-from typing import Any, Optional, Iterable, Tuple
+
+
+__author__ = "Howard C Lovatt"
+__copyright__ = "Howard C Lovatt, 2020 onwards."
+__license__ = "MIT https://opensource.org/licenses/MIT (as used by MicroPython)."
+__version__ = "4.0.0"  # Version set by https://github.com/hlovatt/tag2ver
+
+
+
+from abc import abstractmethod
+from typing import Protocol, Iterable, AnyStr, runtime_checkable, Optional, TypeVar, Tuple, Final
+
+from uarray import array
+
+
+@runtime_checkable
+class _IOBase(Protocol):
+    """
+    A `Protocol` (structurally typed) for an IOStream.
+    """
+
+    __slots__ = ()
+
+    @abstractmethod
+    def __enter__(self) -> "_IOBase": ...
+    
+    @abstractmethod
+    def __exit__(self) -> None: ...
+    
+    @abstractmethod
+    def __next__(self) -> AnyStr: ...
+    
+    @abstractmethod
+    def close(self) -> None: ...
+    
+    @abstractmethod
+    def read(self, size: Optional[int] = -1) -> Optional[bytes]: ...
+    
+    @abstractmethod
+    def readinto(self, b: _AnyWritableBuf) -> int: ...
+    
+    @abstractmethod
+    def readline(self, size: int = -1) -> AnyStr: ...
+    
+    @abstractmethod
+    def write(self, b: _AnyReadableBuf) -> int: ...
+    
+    @abstractmethod
+    def flush(self) -> None: ...
+    
+    @abstractmethod
+    def seek(self, offset: int, whence: int = 0) -> int: ...
+    
+    @abstractmethod
+    def tell(self) -> int: ...
+
+
+
+_AnyWritableBuf = TypeVar('_AnyWritableBuf', bytearray, array, memoryview)
+"""
+Type that allows bytearray, array, or memoryview, but only one of these and not a mixture in a single declaration.
+"""
+
+
+
+
+_AnyReadableBuf = TypeVar('_AnyReadableBuf', bytearray, array, memoryview, bytes)
+"""
+Type that allows bytearray, array, memoryview, or bytes, 
+but only one of these and not a mixture in a single declaration.
+"""
+
+
+
+
+def open(stream: _IOBase, /, *, flags: int = 0, pagesize: int = 0, cachesize: int = 0, minkeypage: int = 0) -> "_BTree":
+   """
+   Open a database from a random-access `stream` (like an open file). All
+   other parameters are optional and keyword-only, and allow to tweak advanced
+   parameters of the database operation (most users will not need them):
+   
+   * *flags* - Currently unused.
+   * *pagesize* - Page size used for the nodes in BTree. Acceptable range
+     is 512-65536. If 0, a port-specific default will be used, optimized for
+     port's memory usage and/or performance.
+   * *cachesize* - Suggested memory cache size in bytes. For a
+     board with enough memory using larger values may improve performance.
+     Cache policy is as follows: entire cache is not allocated at once;
+     instead, accessing a new page in database will allocate a memory buffer
+     for it, until value specified by *cachesize* is reached. Then, these
+     buffers will be managed using LRU (least recently used) policy. More
+     buffers may still be allocated if needed (e.g., if a database contains
+     big keys and/or values). Allocated cache buffers aren't reclaimed.
+   * *minkeypage* - Minimum number of keys to store per page. Default value
+     of 0 equivalent to 2.
+   
+   Returns a BTree object, which implements a dictionary protocol (set
+   of methods), and some additional methods described below.
+   """
+
+
+INCL: Final[int] = ...
+"""
+A flag for `keys()`, `values()`, `items()` methods to specify that
+   scanning should be inclusive of the end key.
+"""
+
+
+
+
+DESC: Final[int] = ...
+"""
+A flag for `keys()`, `values()`, `items()` methods to specify that
+   scanning should be in descending direction of keys.
+"""
+
+
 
 
 class _BTree:
-    def close(self) -> None:
-        """Close the database. It's mandatory to close the database at the end of
-        processing, as some unwritten data may be still in the cache. Note that
-        this does not close underlying stream with which the database was opened,
-        it should be closed separately (which is also mandatory to make sure that
-        data flushed from buffer to the underlying storage).
-        """
-        ...
+   """
 
-    def flush(self) -> None:
-        """Flush any data in cache to the underlying stream."""
-        ...
-
-    def __getitem__(self, key: bytes) -> bytes:
-        ...
-
-    def get(self, key: bytes, default: Optional[bytes] = None) -> Optional[bytes]:
-        ...
-
-    def __setitem__(self, key: bytes, val: bytes) -> None:
-        ...
-
-    def __detitem__(self, key: bytes) -> None:
-        ...
-
-    def __contains__(self, key: bytes) -> bool:
-        ...
-
-    def __iter__(self) -> Iterable[bytes]:
-        """A BTree object can be iterated over directly (similar to a dictionary)
-        to get access to all keys in order.
-        """
-        ...
-
-    def keys(self, start_key: bytes = None, end_key: bytes = None, flags: int = None) -> Iterable[bytes]:
-        ...
-
-    def values(self, start_key: bytes = None, end_key: bytes = None, flags: int = None) -> Iterable[bytes]:
-        ...
-
-    def items(self, start_key: bytes = None, end_key: bytes = None, flags: int = None) -> Iterable[Tuple[bytes, bytes]]:
-        ...
+   """
 
 
-def open(stream: Any, *, flags: int = 0, pagesize: int = 0, cachesize: int = 0,
-         minkeypage: int = 0) -> _BTree:
-    ...
+
+   def close(self) -> None:
+      """
+      Close the database. It's mandatory to close the database at the end of
+      processing, as some unwritten data may be still in the cache. Note that
+      this does not close underlying stream with which the database was opened,
+      it should be closed separately (which is also mandatory to make sure that
+      data flushed from buffer to the underlying storage).
+      """
+
+   def flush(self) -> None:
+      """
+      Flush any data in cache to the underlying stream.
+      """
+
+   def __getitem__(self, key: bytes, /) -> bytes:
+      """
+      Standard dictionary methods.
+      """
+
+   def get(self, key: bytes, default: Optional[bytes] = None, /) -> Optional[bytes]:
+      """
+      Standard dictionary methods.
+      """
+
+   def __setitem__(self, key: bytes, val: bytes, /) -> None:
+      """
+      Standard dictionary methods.
+      """
+
+   def __delitem__(self, key: bytes, /) -> None:
+      """
+      Standard dictionary methods.
+      """
+
+   def __contains__(self, key: bytes, /) -> bool:
+      """
+      Standard dictionary methods.
+      """
+
+   def __iter__(self) -> Iterable[bytes]:
+      """
+      A BTree object can be iterated over directly (similar to a dictionary)
+      to get access to all keys in order.
+      """
+
+   
+   def keys(
+      self, 
+      start_key: Optional[bytes] = None, 
+      end_key: Optional[bytes] = None, 
+      flags: int = 0, 
+      /
+   ) -> Iterable[bytes]:
+      """
+      These methods are similar to standard dictionary methods, but also can
+      take optional parameters to iterate over a key sub-range, instead of
+      the entire database. Note that for all 3 methods, *start_key* and
+      *end_key* arguments represent key values. For example, `values()`
+      method will iterate over values corresponding to they key range
+      given. None values for *start_key* means "from the first key", no
+      *end_key* or its value of None means "until the end of database".
+      By default, range is inclusive of *start_key* and exclusive of
+      *end_key*, you can include *end_key* in iteration by passing *flags*
+      of `btree.INCL`. You can iterate in descending key direction
+      by passing *flags* of `btree.DESC`. The flags values can be ORed
+      together.
+      """
+
+   
+   def values(
+      self, 
+      start_key: Optional[bytes] = None, 
+      end_key: Optional[bytes] = None, 
+      flags: int = 0, 
+      /
+   ) -> Iterable[bytes]:
+      """
+      These methods are similar to standard dictionary methods, but also can
+      take optional parameters to iterate over a key sub-range, instead of
+      the entire database. Note that for all 3 methods, *start_key* and
+      *end_key* arguments represent key values. For example, `values()`
+      method will iterate over values corresponding to they key range
+      given. None values for *start_key* means "from the first key", no
+      *end_key* or its value of None means "until the end of database".
+      By default, range is inclusive of *start_key* and exclusive of
+      *end_key*, you can include *end_key* in iteration by passing *flags*
+      of `btree.INCL`. You can iterate in descending key direction
+      by passing *flags* of `btree.DESC`. The flags values can be ORed
+      together.
+      """
+
+   
+   def items(
+      self, 
+      start_key: Optional[bytes] = None, 
+      end_key: Optional[bytes] = None, 
+      flags: int = 0, 
+      /
+   ) -> Iterable[Tuple[bytes, bytes]]:
+      """
+      These methods are similar to standard dictionary methods, but also can
+      take optional parameters to iterate over a key sub-range, instead of
+      the entire database. Note that for all 3 methods, *start_key* and
+      *end_key* arguments represent key values. For example, `values()`
+      method will iterate over values corresponding to they key range
+      given. None values for *start_key* means "from the first key", no
+      *end_key* or its value of None means "until the end of database".
+      By default, range is inclusive of *start_key* and exclusive of
+      *end_key*, you can include *end_key* in iteration by passing *flags*
+      of `btree.INCL`. You can iterate in descending key direction
+      by passing *flags* of `btree.DESC`. The flags values can be ORed
+      together.
+      """
 
 
-INCL: int
-DESC: int
