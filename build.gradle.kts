5fc61e7986aafa535871eda1842cfc9c92e4c9a0
Ilia (Elias) Motornyi <elijah.mot@gmail.com>
2024-07-26 22:22:14 +0300

    Gradle small fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation("org.java-websocket:Java-WebSocket:1.5.5")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.3")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.3")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks {
