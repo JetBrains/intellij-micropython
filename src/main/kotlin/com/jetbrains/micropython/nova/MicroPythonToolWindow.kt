de454e408f91c9fa19213715224ac277d4a0ebce
Ilia (Elias) Motornyi <elijah.mot@gmail.com>
2024-07-28 23:24:04 +0300

    NOVA - Connect/disconnect control & small UI fixes
    Fix #309

--- src/main/kotlin/com/jetbrains/micropython/nova/MicroPythonToolWindow.kt
+++ src/main/kotlin/com/jetbrains/micropython/nova/MicroPythonToolWindow.kt
@@ -1,67 +1,45 @@
 package com.jetbrains.micropython.nova
 
-import com.intellij.icons.AllIcons
-import com.intellij.notification.Notification
-import com.intellij.notification.NotificationType
-import com.intellij.notification.Notifications
-import com.intellij.openapi.actionSystem.*
-import com.intellij.openapi.application.EDT
-import com.intellij.openapi.application.readAction
-import com.intellij.openapi.fileEditor.FileDocumentManager
-import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.actionSystem.ActionGroup
+import com.intellij.openapi.actionSystem.ActionManager
+import com.intellij.openapi.actionSystem.ActionPlaces
 import com.intellij.openapi.project.DumbAware
-import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.roots.ex.ProjectRootManagerEx
 import com.intellij.openapi.util.Disposer
-import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
-import com.intellij.openapi.wm.ToolWindowManager
-import com.intellij.platform.ide.progress.runWithModalProgressBlocking
+import com.intellij.terminal.JBTerminalWidget
 import com.intellij.ui.content.ContentFactory
-import com.intellij.util.asSafely
 import com.intellij.util.ui.components.BorderLayoutPanel
 import com.jediterm.terminal.TerminalMode
 import com.jediterm.terminal.TtyConnector
-import com.jediterm.terminal.ui.JediTermWidget
 import com.jetbrains.rd.util.URI
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.TimeoutCancellationException
-import kotlinx.coroutines.withContext
-import org.jetbrains.annotations.NonNls
 import org.jetbrains.plugins.terminal.JBTerminalSystemSettingsProvider
-import java.io.IOException
-import javax.swing.Icon
 import javax.swing.JComponent
-import kotlin.coroutines.cancellation.CancellationException
 
-private const val NOTIFICATION_GROUP = "Micropython"
+internal const val NOTIFICATION_GROUP = "Micropython"
 
 class MicroPythonToolWindow : ToolWindowFactory, DumbAware {
 
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
         val newDisposable = Disposer.newDisposable(toolWindow.disposable, "Webrepl")
-        val comm = WebSocketComm {
-            it.printStackTrace()
-        }
-        Disposer.register(newDisposable, comm)
 
-        val jediTermWidget = jediTermWidget(comm.ttyConnector)
-        val termContent = ContentFactory.getInstance().createContent(jediTermWidget, "REPL", true)
-        val fileSystemWidget = FileSystemWidget(project, comm)
+        val fileSystemWidget = FileSystemWidget(project, newDisposable)
         val fileSystemContent = ContentFactory.getInstance().createContent(fileSystemWidget, "File System", true)
+        fileSystemContent.setDisposer(newDisposable)
+        fileSystemWidget.setConnectionParams(URI("ws://192.168.50.68:8266"), "passwd")
         toolWindow.contentManager.addContent(fileSystemContent)
+
+        val jediTermWidget = jediTermWidget(project, newDisposable, fileSystemWidget.ttyConnector)
+        val termContent = ContentFactory.getInstance().createContent(jediTermWidget, "REPL", true)
+        termContent.setDisposer(newDisposable)
         toolWindow.contentManager.addContent(termContent)
-        runWithModalProgressBlocking(project, "Initializing WebREPL") {
-            comm.connect(URI("ws://192.168.50.68:8266"), "passwd")
-            fileSystemWidget.refresh()
-        }
     }
 
-    private fun jediTermWidget(connector: TtyConnector): JComponent {
+    private fun jediTermWidget(project: Project, disposable: Disposable, connector: TtyConnector): JComponent {
         val mySettingsProvider = JBTerminalSystemSettingsProvider()
-        val terminal = JediTermWidget(mySettingsProvider)
+        val terminal = JBTerminalWidget(project, mySettingsProvider, disposable)
         terminal.isEnabled = false
         with(terminal.terminal) {
             setModeEnabled(TerminalMode.ANSI, true)
@@ -83,87 +61,3 @@ class MicroPythonToolWindow : ToolWindowFactory, DumbAware {
 
 }
 
-abstract class ReplAction(text: String, icon: Icon) : DumbAwareAction(text, "", icon) {
-
-    abstract val actionDescription: String
-
-    protected fun isFileEditorActive(e: AnActionEvent): Boolean {
-        return e.project?.let { FileEditorManager.getInstance(it).selectedEditor } != null
-    }
-
-    @Throws(IOException::class, TimeoutCancellationException::class, CancellationException::class)
-    abstract suspend fun performAction(fileSystemWidget: FileSystemWidget)
-
-    override fun actionPerformed(e: AnActionEvent) {
-        val project = e.project ?: return
-        val fileSystemWidget = fileSystemWidget(project) ?: return
-        fileSystemWidget.isVisible = false
-        try {
-        runWithModalProgressBlocking(project, "Board data exchange...") {
-            var error: String? = null
-            try {
-                performAction(fileSystemWidget)
-            } catch (e: TimeoutCancellationException) {
-                error = "$actionDescription timed out"
-            } catch (e: CancellationException) {
-                error = "$actionDescription cancelled"
-            } catch (e: IOException) {
-                error = "$actionDescription I/O error - ${e.localizedMessage ?: e.message ?: "No message"}"
-            } catch (e: Exception) {
-                error = e.localizedMessage ?: e.message
-                error = if (error.isNullOrBlank()) "$actionDescription error - ${e::class.simpleName}"
-                else "$actionDescription error - ${e::class.simpleName}: $error"
-            }
-            if (!error.isNullOrBlank()) {
-                Notifications.Bus.notify(Notification(NOTIFICATION_GROUP, error, NotificationType.ERROR), project)
-            }
-        }
-        } finally {
-            fileSystemWidget.isVisible = true
-        }
-    }
-
-    private fun fileSystemWidget(project: Project?): FileSystemWidget? {
-        return ToolWindowManager.getInstance(project ?: return null)
-            .getToolWindow("com.jetbrains.micropython.nova.MicroPythonToolWindow")
-            ?.contentManager
-            ?.contents
-            ?.firstNotNullOfOrNull { it.component.asSafely<FileSystemWidget>() }
-    }
-
-    protected  fun fileSystemWidget(e: AnActionEvent): FileSystemWidget? = fileSystemWidget(e.project)
-
-}
-
-class Refresh : ReplAction("Refresh", AllIcons.Actions.Refresh) {
-
-    override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.EDT
-
-    override val actionDescription: String = "Refresh"
-
-    override suspend fun performAction(fileSystemWidget: FileSystemWidget) = fileSystemWidget.refresh()
-}
-
-open class UploadFile(text: String = "Upload File", icon: Icon = AllIcons.Actions.Upload) : ReplAction(text, icon) {
-//todo move out of MPY toolwindow
-    override val actionDescription: String = "Upload"
-
-    override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.EDT
-
-    override suspend fun performAction(fileSystemWidget: FileSystemWidget) {
-        val file = withContext(Dispatchers.EDT) {
-            FileDocumentManager.getInstance().saveAllDocuments()
-            FileEditorManager.getInstance(fileSystemWidget.project).selectedEditor
-                ?.file
-        } ?: return
-        val relativeName = readAction<@NonNls String?> {
-            ProjectRootManagerEx.getInstance(fileSystemWidget.project)
-                .contentRoots
-                .firstNotNullOfOrNull {
-                    VfsUtil.getRelativePath(file, it)
-                }
-        } ?: return
-        fileSystemWidget.comm.upload(relativeName, file.contentsToByteArray())
-        fileSystemWidget.refresh()
-    }
-}
---
+++ src/main/kotlin/com/jetbrains/micropython/nova/actions.kt
@@ -0,0 +1,252 @@
+package com.jetbrains.micropython.nova
+
+import com.intellij.icons.AllIcons
+import com.intellij.ide.ActivityTracker
+import com.intellij.notification.Notification
+import com.intellij.notification.NotificationType
+import com.intellij.notification.Notifications
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.application.EDT
+import com.intellij.openapi.application.readAction
+import com.intellij.openapi.diagnostic.thisLogger
+import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.fileEditor.TextEditor
+import com.intellij.openapi.fileTypes.FileTypeRegistry
+import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ex.ProjectRootManagerEx
+import com.intellij.openapi.vfs.VfsUtil
+import com.intellij.openapi.wm.ToolWindowManager
+import com.intellij.platform.ide.progress.runWithModalProgressBlocking
+import com.intellij.testFramework.LightVirtualFile
+import com.intellij.util.asSafely
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.withContext
+import org.jetbrains.annotations.NonNls
+import java.io.IOException
+import java.nio.charset.StandardCharsets
+import javax.swing.Icon
+import kotlin.coroutines.cancellation.CancellationException
+
+abstract class ReplAction(text: String, icon: Icon) : DumbAwareAction(text, "", icon) {
+
+    abstract val actionDescription: String
+
+    protected fun isFileEditorActive(e: AnActionEvent): Boolean {
+        return e.project?.let { FileEditorManager.getInstance(it).selectedEditor } != null
+    }
+
+    @Throws(IOException::class, TimeoutCancellationException::class, CancellationException::class)
+    abstract suspend fun performAction(fileSystemWidget: FileSystemWidget)
+
+    override fun actionPerformed(e: AnActionEvent) {
+        val project = e.project ?: return
+        val fileSystemWidget = fileSystemWidget(project) ?: return
+        runWithModalProgressBlocking(project, "Board data exchange...") {
+            var error: String? = null
+            try {
+                performAction(fileSystemWidget)
+            } catch (e: TimeoutCancellationException) {
+                error = "$actionDescription timed out"
+                thisLogger().info(error, e)
+            } catch (e: CancellationException) {
+                error = "$actionDescription cancelled"
+                thisLogger().info(error, e)
+            } catch (e: IOException) {
+                error = "$actionDescription I/O error - ${e.localizedMessage ?: e.message ?: "No message"}"
+                thisLogger().info(error, e)
+            } catch (e: Exception) {
+                error = e.localizedMessage ?: e.message
+                error = if (error.isNullOrBlank()) "$actionDescription error - ${e::class.simpleName}"
+                else "$actionDescription error - ${e::class.simpleName}: $error"
+                thisLogger().error(error, e)
+            }
+            if (!error.isNullOrBlank()) {
+                Notifications.Bus.notify(Notification(NOTIFICATION_GROUP, error, NotificationType.ERROR), project)
+            }
+        }
+    }
+
+    private fun fileSystemWidget(project: Project?): FileSystemWidget? {
+        return ToolWindowManager.getInstance(project ?: return null)
+            .getToolWindow("com.jetbrains.micropython.nova.MicroPythonToolWindow")
+            ?.contentManager
+            ?.contents
+            ?.firstNotNullOfOrNull { it.component.asSafely<FileSystemWidget>() }
+    }
+
+    protected fun fileSystemWidget(e: AnActionEvent): FileSystemWidget? = fileSystemWidget(e.project)
+    fun enableIfConnected(e: AnActionEvent) {
+        if (fileSystemWidget(e)?.state != State.CONNECTED) {
+            e.presentation.isEnabled = false
+        }
+    }
+
+}
+
+class Refresh : ReplAction("Refresh", AllIcons.Actions.Refresh) {
+
+    override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.EDT
+
+    override val actionDescription: String = "Refresh"
+
+    override fun update(e: AnActionEvent) = enableIfConnected(e)
+
+    override suspend fun performAction(fileSystemWidget: FileSystemWidget) = fileSystemWidget.refresh()
+}
+
+class Disconnect(text: String = "Disconnect", icon: Icon = AllIcons.General.Remove) : ReplAction(text, icon) {
+
+    override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.EDT
+
+    override val actionDescription: String = "Disconnect"
+
+    override suspend fun performAction(fileSystemWidget: FileSystemWidget) = fileSystemWidget.disconnect()
+
+    override fun update(e: AnActionEvent) {
+        if (fileSystemWidget(e)?.state != State.CONNECTED) {
+            e.presentation.isEnabledAndVisible = false
+        }
+    }
+}
+
+class Connect(text: String = "Connect", icon: Icon = AllIcons.General.ArrowUp) : ReplAction(text, icon) {
+
+    override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.EDT
+
+    override val actionDescription: String = "Connect"
+
+    override suspend fun performAction(fileSystemWidget: FileSystemWidget) {
+        if (fileSystemWidget.state != State.CONNECTED) {
+            fileSystemWidget.connect()
+            fileSystemWidget.refresh()
+            ActivityTracker.getInstance().inc()
+        }
+    }
+
+    override fun update(e: AnActionEvent) {
+        e.presentation.isEnabledAndVisible = when (fileSystemWidget(e)?.state) {
+            State.DISCONNECTING, State.DISCONNECTED, null -> true
+            State.CONNECTING, State.CONNECTED, State.TTY_DETACHED -> false
+        }
+    }
+}
+
+class DeleteFile : ReplAction("Delete File", AllIcons.General.Remove) {
+
+    override val actionDescription: String = "Delete"
+
+    override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.EDT
+
+    override suspend fun performAction(fileSystemWidget: FileSystemWidget) {
+        try {
+            fileSystemWidget.deleteCurrent()
+        } finally {
+            fileSystemWidget.refresh()
+        }
+    }
+
+    override fun update(e: AnActionEvent) {
+        if (fileSystemWidget(e)?.state != State.CONNECTED) {
+            e.presentation.isEnabled = false
+            return
+        }
+        val selectedFile = fileSystemWidget(e)?.selectedFile()
+        e.presentation.isEnabled = selectedFile?.fullName !in listOf("/", null)
+        e.presentation.text = if (selectedFile is DirNode) "Delete Folder" else "Delete File"
+    }
+}
+
+class InstantRun : ReplAction("Instant Run", AllIcons.Actions.Rerun) {
+
+    override val actionDescription: String = "Run code"
+
+    override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.EDT
+
+    override fun update(e: AnActionEvent) {
+        if (fileSystemWidget(e)?.state != State.CONNECTED) {
+            e.presentation.isEnabled = false
+            return
+        }
+        e.presentation.isEnabled = isFileEditorActive(e)
+    }
+
+    override suspend fun performAction(fileSystemWidget: FileSystemWidget) {
+        val code = withContext(Dispatchers.EDT) {
+            FileEditorManager.getInstance(fileSystemWidget.project).selectedEditor.asSafely<TextEditor>()?.editor?.document?.text
+        }
+        if (code != null) {
+            fileSystemWidget.instantRun(code)
+        }
+    }
+}
+
+class OpenMpyFile : ReplAction("Open file", AllIcons.Actions.MenuOpen) {
+
+    override val actionDescription: String = "Open file"
+
+    override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.EDT
+
+    override fun update(e: AnActionEvent) {
+        if (fileSystemWidget(e)?.state != State.CONNECTED) {
+            e.presentation.isEnabledAndVisible = false
+            return
+        }
+        e.presentation.isEnabledAndVisible = fileSystemWidget(e)?.selectedFile() is FileNode
+    }
+
+    override suspend fun performAction(fileSystemWidget: FileSystemWidget) {
+        fun fileReadCommand(name: String) = """
+with open('$name','rb') as f:
+    while 1:
+          b=f.read(50)
+          if not b:break
+          print(b.hex())
+"""
+
+        val selectedFile = withContext(Dispatchers.EDT) {
+            fileSystemWidget.selectedFile()
+        }
+        if (selectedFile !is FileNode) return
+        val result = fileSystemWidget.blindExecute(fileReadCommand(selectedFile.fullName)).extractSingleResponse()
+        val text =
+            result.filter { it.isDigit() || it in 'a'..'f' || it in 'A'..'F' }.chunked(2).map { it.toInt(16).toByte() }
+                .toByteArray().toString(StandardCharsets.UTF_8)
+        withContext(Dispatchers.EDT) {
+            val fileType = FileTypeRegistry.getInstance().getFileTypeByFileName(selectedFile.name)
+            val infoFile = LightVirtualFile("micropython: ${selectedFile.fullName}", fileType, text)
+            infoFile.isWritable = false
+            FileEditorManager.getInstance(fileSystemWidget.project).openFile(infoFile, false)
+        }
+    }
+}
+
+open class UploadFile(text: String = "Upload File", icon: Icon = AllIcons.Actions.Upload) : ReplAction(text, icon) {
+    //todo move out of MPY toolwindow
+    override val actionDescription: String = "Upload"
+
+    override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.EDT
+
+    override suspend fun performAction(fileSystemWidget: FileSystemWidget) {
+        val file = withContext(Dispatchers.EDT) {
+            FileDocumentManager.getInstance().saveAllDocuments()
+            FileEditorManager.getInstance(fileSystemWidget.project).selectedEditor
+                ?.file
+        } ?: return
+        val relativeName = readAction<@NonNls String?> {
+            ProjectRootManagerEx.getInstance(fileSystemWidget.project)
+                .contentRoots
+                .firstNotNullOfOrNull {
+                    VfsUtil.getRelativePath(file, it)
+                }
+        } ?: return
+        try {
+            fileSystemWidget.upload(relativeName, file.contentsToByteArray())
+        } finally {
+            fileSystemWidget.refresh()
+        }
+    }
+}
